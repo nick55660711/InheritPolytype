22b5c0b00369c98999b922fdbb369e2cc1a16a86
nick55660711 <63327482+nick55660711@users.noreply.github.com>
2020-06-23 10:36:15 +0800

    10100

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene("遊戲場景");
+
+        }
+
+    }
+}
